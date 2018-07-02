f3ee9e08f0678eb0565b201fb62feb54a28cb090
ads-26 <40528745+ads-26@users.noreply.github.com>
2018-07-02 17:51:07 +0530

    Update models.py

--- file/models.py
+++ file/models.py
@@ -5,8 +5,3 @@ class File(models.Model):
   file_url = models.CharField (max_length=200 )
   name = models.CharField(max_length=20)
 
-# Create your models here.
-# username= admin1
-#password = pass1234
-#aditi_
-#aditi1234
